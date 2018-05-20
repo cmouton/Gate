cecabc6a3173289cf7d920a10e38884843d47d68
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2018-05-20 02:25:25 +0200

    Added a script to build Geant4.

---
+++ travis/geant4_build.sh
@@ -0,0 +1,26 @@
+# Parameter:
+# version: Geant4 version to be downloaded, compiled and installed.
+# Warning : only the version "10.00.p02", "10.01" and "10.01.p01" have been tested with this script.
+# Example:
+# /bin/bash geant4_build.sh 10.01
+
+cd $HOME
+rm -rf geant4.$1
+mkdir geant4.$1
+cd geant4.$1/
+mkdir geant4.$1-build
+mkdir geant4.$1-install
+
+# Retrieve the source.
+wget http://geant4.web.cern.ch/geant4/support/source/geant4.$1.tar.gz
+tar xvzf geant4.$1.tar.gz
+
+# Build.
+cd geant4.$1-build/
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/geant4.$1/geant4.$1-install/ \
+      -DGEANT4_INSTALL_DATA:BOOL=ON ../geant4.$1
+make -j8
+
+# Install.
+make install
+source $HOME/geant4.$1/geant4.$1-install/bin/geant4.sh
