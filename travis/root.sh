435250b607838e8c6e338a8170b0ad09ae82e349
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2018-05-21 22:40:25 +0200

    Created a script for root installation

---
+++ travis/root.sh
@@ -0,0 +1,7 @@
+rm -rf $HOME/root
+mkdir $HOME/root
+cd $HOME/root
+wget https://root.cern.ch/download/root_v6.10.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz 
+tar -xvzf root_v6.10.06.Linux-ubuntu14-x86_64-gcc4.8.tar.gz
+mv root root_v6.10.06
+
