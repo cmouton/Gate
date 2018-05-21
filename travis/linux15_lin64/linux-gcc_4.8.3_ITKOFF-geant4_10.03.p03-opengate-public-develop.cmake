a49941a659fa352f0f3ed481e97eeb16774b94b7
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2018-05-21 19:07:01 +0200

    Switched from cretis repository to official Gate repository

--- travis/linux15_lin64/linux-gcc_4.8.3_ITKOFF-geant4_10.03.p03-opengate-public-develop.cmake
+++ travis/linux15_lin64/linux-gcc_4.8.3_ITKOFF-geant4_10.03.p03-opengate-public-develop.cmake
@@ -5,9 +5,9 @@ set(GEANT4_VERSION "10.03.p03")
 # Edit the branch name.
 set(CTEST_GIT_BRANCH "develop")
 # Edit the name of the repository to be cloned and tested.
-set(CTEST_GIT_REPOSITORY "opengate-creatis")
+set(CTEST_GIT_REPOSITORY "Gate")
 # Edit the URL of the repository.
-set(CTEST_GIT_URL "ssh://gitolite@dashboard-opengate-creatis.git.creatis.insa-lyon.fr")
+set(CTEST_GIT_URL "https://github.com/OpenGATE")
 
 # Specific options
 set(CTEST_CONFIGURE_COMMAND_OPTIONS "\"-DGATE_USE_ITK:BOOL=OFF\"")
